52a771e61b67af3cb52b86215c382bef725d51b0
MusaA-GitHu <musaabdusamed@gmail.com>
2026-06-09 03:19:59 -1200

    Add minOperations method to Solution class
    Implement minOperations method to calculate minimum operations needed to make the sum of the array divisible by k.

--- Leetcode/Array/String/minimum-operations-to-make-array-sum-divisible-by-k.py
+++ Leetcode/Array/String/minimum-operations-to-make-array-sum-divisible-by-k.py
@@ -1,1 +1,12 @@
-
+#Solution is here 👌 
+class Solution(object):
+    def minOperations(self, nums, k):
+        """
+        :type nums: List[int]
+        :type k: int
+        :rtype: int
+        """
+        total_sum=sum(nums)
+        t=total_sum%k
+        return t
+        
